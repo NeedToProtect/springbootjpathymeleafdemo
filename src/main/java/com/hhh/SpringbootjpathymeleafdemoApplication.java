9190c8f475531506141510bcac327dedc246041f
hhh-java <578145591@qq.com>
2019-07-10 15:18:45 +0800

    Initial commit

---
+++ src/main/java/com/hhh/SpringbootjpathymeleafdemoApplication.java
@@ -0,0 +1,13 @@
+package com.hhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjpathymeleafdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjpathymeleafdemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hhh/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hhh.dao;
+
+import com.hhh.bean.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByName(String name);
+    User findByNameAndAge(String name,Integer age);
+
+}
+
